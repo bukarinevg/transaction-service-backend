4198c83ef69916488aeac10bbd4543fcf21ec435
Evgenii Bukarin <bukarinevgeni@gmail.com>
2025-03-25 01:15:50 +0200

    Add database migrations for project, user, payment, and user balance tables

---
+++ database/migrations/2025_03_24_225928_create_user_balance_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_balance', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('user')->cascadeOnDelete();
+            $table->decimal('balance_rub', 15, 2)->default(0);
+            $table->decimal('balance_usd', 15, 2)->default(0);
+            $table->decimal('balance_kzt', 15, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_balance');
+    }
+};
